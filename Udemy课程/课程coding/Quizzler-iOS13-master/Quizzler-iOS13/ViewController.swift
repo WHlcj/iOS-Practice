8d5f77fe6cc0bb6de58b6a1d97b7b562da97a4b7
WHlcj <1369352274@qq.com>
2023-04-27 10:00:47 +0800

    2023.4.27

---
+++ Udemy课程/课程coding/Quizzler-iOS13-master/Quizzler-iOS13/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+}
+

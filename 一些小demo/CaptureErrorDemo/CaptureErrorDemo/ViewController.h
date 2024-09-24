6cf6ddc6dea776c8896d7dd6daecdbb54bb27621
Whlcj <1369352274@qq.com>
2024-09-24 23:16:15 +0800

    增加全局异常处理Demo

---
+++ 一些小demo/CaptureErrorDemo/CaptureErrorDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  CaptureErrorDemo
+//
+//  Created by Changjun Li on 2024/9/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 一些小demo/CaptureErrorDemo/CaptureErrorDemo/ViewController.m
@@ -0,0 +1,52 @@
+//
+//  ViewController.m
+//  CaptureErrorDemo
+//
+//  Created by Changjun Li on 2024/9/24.
+//
+
+#import "ViewController.h"
+
+void uploadCrashLog(NSString *crashInfo) {
+    // 将 crashInfo 上传到你的服务器
+    NSLog(@"Uploading crash log: %@", crashInfo);
+}
+
+NSString* getExceptionInfo(NSException *exception) {
+    NSString *name = exception.name;
+    NSString *reason = exception.reason ?: @"Unknown reason";
+    NSString *userInfo = exception.userInfo ? [exception.userInfo description] : @"No additional info";
+    NSString *callStack = [[exception callStackSymbols] componentsJoinedByString:@"\n"];
+    
+    NSString *crashLog = [NSString stringWithFormat:@"Crash Name: %@\nReason: %@\nUser Info: %@\nCall Stack: %@", name, reason, userInfo, callStack];
+    return crashLog;
+}
+
+void handleException(NSException *exception) {
+    NSString *crashInfo = getExceptionInfo(exception);
+    uploadCrashLog(crashInfo);
+}
+
+void setupCrashHandler(void) {
+    NSSetUncaughtExceptionHandler(&handleException);
+}
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // 设置全局异常处理器
+    setupCrashHandler();
+
+    // 模拟一个崩溃
+    @throw [NSException exceptionWithName:@"TestException" reason:@"Testing crash" userInfo:nil];
+}
+
+
+@end

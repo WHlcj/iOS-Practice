597e62cabe45724bbd4843ec266e1ff5c006dd61
Whlcj <1369352274@qq.com>
2024-09-28 15:36:59 +0800

    完善blockDemo

--- 一些小demo/blockDemo/blockDemo/ViewController.m
+++ 一些小demo/blockDemo/blockDemo/ViewController.m
@@ -16,24 +16,53 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    __block int count = 10;
-    // 定义一个栈上的 Block
-    void (^myBlock)(void) = ^{
-        // Block 内访问和修改 __block 变量
-        NSLog(@"Inside Block - Before: %d", count);
-        count += 5;
-        NSLog(@"Inside Block - After: %d", count);
+    [self test];
+    [self test2];
+}
+
+- (void)test{
+    int age = 10;
+    
+    void (^block1)(void) = ^{
+        NSLog(@"-----");
+    };
+    NSLog(@"NSGlobalBlock：%@",[block1 class]);
+    
+    NSLog(@"NSStackBlock：%@",[^{
+        NSLog(@"----%d",age);
+    } class]);
+    
+    NSLog(@"NSMallocBlock：%@",[[^{
+        NSLog(@"----%d",age);
+    } copy] class]);
+    
+    void (^block4)(void) = ^{
+        NSLog(@"----%d",age);
     };
     
+    NSLog(@"NSMallocBlock：%@", [block4 class]);
+}
+
+- (void)test2 {
+    __block int count = 10;
+    
     // 执行栈上的 Block
+    NSLog(@"myBlock：%@", [^{
+        count += 5;
+    } class]);
     NSLog(@"Before Block execution - count: %d", count);
-    myBlock();
+    ^{
+        count += 5;
+    }();
     NSLog(@"After Block execution - count: %d", count);
     
     // 将 Block 复制到堆上
-    void (^heapBlock)(void) = [myBlock copy];
+    void (^heapBlock)(void) = [^{
+        count += 5;
+    } copy];
     
     // 执行堆上的 Block
+    NSLog(@"heapBlock：%@", [heapBlock class]);
     NSLog(@"Before Heap Block execution - count: %d", count);
     heapBlock();
     NSLog(@"After Heap Block execution - count: %d", count);
